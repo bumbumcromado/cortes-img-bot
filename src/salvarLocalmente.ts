afc29bf6a2b4c8c743a64385f80102b1bd50a158
bumbumcromado <paulo.jemile@gmail.com>
2022-02-10 15:13:32 -0300

    funcionamento post twitter ok

--- src/salvarLocalmente.ts
+++ src/salvarLocalmente.ts
@@ -1,5 +1,10 @@
 import { gerarThumbnail } from "./gerarThumbnail";
-import salvarArquivo from "./salvarArquivo";
+import TwitterApi from 'twitter-api-v2';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+//import salvarArquivo from "./salvarArquivo";
 
 const thumbTeste = {
     nome: 'teste_imgAleatoria',
@@ -13,10 +18,25 @@ const thumbTeste = {
     avatar: 'https://i.imgur.com/L8jx3y5.png'
 };
 
+const userClient = new TwitterApi({
+    appKey: process.env.API_KEY,
+    appSecret: process.env.API_SECRET,
+    accessToken: process.env.ACCESS_TOKEN,
+    accessSecret: process.env.ACCESS_TOKEN_SECRET,
+  });
+  
+  const rwClient = userClient.readWrite;
+
+
 async function start(){
-    console.log('iniciando a geração dos prints');
-    const img = await gerarThumbnail(thumbTeste);
-    await salvarArquivo(img, 'teste');
-    console.log('fim');
+    try {
+        console.log('iniciando a geração dos prints');
+        const img = await gerarThumbnail(thumbTeste);
+        const mediaId = await rwClient.v1.uploadMedia(img,{type: 'png'});
+        const {data: createdTweet} = await rwClient.v2.tweet({media: {media_ids: [mediaId]}});
+        console.log('tweet postado, id: ',createdTweet.id);
+    } catch (error) {
+        console.log(error);
+    }
 }
 start();
